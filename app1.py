a78e1a46a6c20566aafe9818142def5b94ae8a6d
Kaushik Sirvole <skaushikk@gmail.com>
2021-04-28 20:04:23 -0500

    .

--- app1.py
+++ app1.py
@@ -28,8 +28,8 @@ def app():
             st.write('---------------   CASE DOES NOT EXIST   -------------------')
         else:
             st.write(f'Form I-{formno}')
-            st.markdown("<h2 style='text-align: center; color: blue;'>CASE STATUS</h2>",  unsafe_allow_html=True)
-            st.markdown(f"<h3 style='text-align: center; color: black;'>{case_status}</h2>",  unsafe_allow_html=True)
+            st.markdown("<h2 style='text-align: center; color: blue;'>CASE STATUS</h2>", unsafe_allow_html=True)
+            st.markdown(f"<h3 style='text-align: center; color: black;'>{case_status}</h2>", unsafe_allow_html=True)
             st.write('------------------------------------')
             st.write(case_desc)
             er = False
@@ -37,20 +37,33 @@ def app():
     ############################################################################
 
     # range analysis
+    try:
+        _df = user_funcs.load_data(user_funcs.get_filename(series, 0))
+    except FileNotFoundError:
+        _df = user_funcs.load_data(user_funcs.get_filename(series, 1))
+
     with st.beta_expander('Range Analysis', expanded=False):
         if er:
             st.write('---------------   INPUT VALID RECEIPT NUMBER   ------------------- ')
             return
+
+
         rnge = st.number_input('Input Number of Cases to Analyze', value=10000)
         rng_start, rng_end = serial - rnge // 2, serial + rnge // 2
 
-        try:
-            _df = user_funcs.load_data(user_funcs.get_filename(series, 0))
-        except FileNotFoundError:
-            _df = user_funcs.load_data(user_funcs.get_filename(series, 1))
-
         df_window = user_funcs.variable_window(_df, rng_start, rng_end).reset_index()
         df_window = df_window[['ReceiptNo', 'FormNo', 'Status', 'serial']].rename(columns={'serial': 'Serial'})
+
+        df_window_series = df_window[df_window['FormNo'] == formno]
+        series_total = df_window_series.shape[0]
+        df_window_series['Status'] = df_window_series['Status'].apply(user_funcs.rename_status)
+        df_window_series_group = df_window_series.groupby('Status').count()['Serial'].reset_index().rename(
+            columns={'Serial': 'Count'})
+
+        df_window_series_group['Ratio'] = df_window_series_group.Count / series_total
+        df_window_series_group['Percent'] = df_window_series_group['Ratio'].apply(lambda x: "{:.0%}".format(x))
+
+
 
         st.header('Number of cases by the Form # (Application type)')
         col1, col2 = st.beta_columns((2, 1))
@@ -63,21 +76,8 @@ def app():
         with col2:
             st.dataframe(df_window_counts, width=1024)
 
-        # alt_chart = alt.Chart(df_window_counts).mark_bar().encode(x='FormNo',
-        #                                                           y='Count', color=alt.Color('FormNo:N')).interactive()
-        # st.write(alt_chart)
-
-        df_window_series = df_window[df_window['FormNo'] == formno]
-        # st.dataframe(df_window_series)
-        series_total = df_window_series.shape[0]
-        df_window_series['Status'] = df_window_series['Status'].apply(user_funcs.rename_status)
-        df_window_series_group = df_window_series.groupby('Status').count()['Serial'].reset_index().rename(
-            columns={'Serial': 'Count'})
-
-        df_window_series_group['Ratio'] = df_window_series_group.Count / series_total
-        df_window_series_group['Percent'] = df_window_series_group['Ratio'].apply(lambda x: "{:.0%}".format(x))
-
         st.header('Distrubution of the cases by STATUS')
+
         col3, col4 = st.beta_columns((1, 1))
         with col3:
             fig, ax1 = plt.subplots()
@@ -90,7 +90,7 @@ def app():
         approved = df_window_series_group.loc[df_window_series_group.Status == 'Approved', 'Percent']
         pending = df_window_series_group.loc[df_window_series_group.Status.str.contains('Pending'), 'Ratio']
         st.header('Approval Ratio')
-        summary1= f"{approved.values[0]} of the similar cases are APPROVED."
+        summary1 = f"{approved.values[0]} of the similar cases are APPROVED."
         summary2 = f"{'{:.0%}'.format(pending.sum())} of cases are still PENDING."
 
         st.markdown(f"<h2 style='text-align: center; color: green;'>{summary1}</h2>", unsafe_allow_html=True)
